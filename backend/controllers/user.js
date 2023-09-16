ad89c3989db99a34a92819aae84e17bd31b69fa6
Laurent SIMON <simon.laurent208@gmail.com>
2023-09-16 15:35:02 +0200

    améliroation sécurité et ajout de images dans .gitignore

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,9 +26,9 @@ exports.login = (req, res, next) => {
         bcrypt.compare(req.body.password, user.password)
             .then(valid => {
                 if (!valid) {
-                     res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                     return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                 }
-                res.status(200).json({
+                return res.status(200).json({
                     userId: user._id,
                     token: jwt.sign(
                         { userId: user._id},
